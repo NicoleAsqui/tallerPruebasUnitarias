3d5581d33cae0ad92b66628b78e97df9b1e6c92f
NicoleAsqui <56647127+NicoleAsqui@users.noreply.github.com>
2020-01-21 00:36:07 -0500

    Update EmployeeTest.java
    Agregame al grupo porfis

--- src/tallerpruebas/EmployeeTest.java
+++ src/tallerpruebas/EmployeeTest.java
@@ -12,40 +12,42 @@ class EmployeeTest {
 	 * with the type of employee and if it receives the extra 10% of salary
 	 * if the month is odd.
 	 */
+	// Test en caso que Employee tenga un salario de salario de 400F con una moneda en Euros, un porcentaje de bonificacion del 80% y sea un trabajador
 	@Test
 	void testCs1() {
 		Employee e = new Employee(400F,"EUR",0.8F,EmployeeType.Worker);
 		float salario = e.cs();
 		assertEquals(444.33F,salario,0.01F);
-	}	
+	}
+	// Test en caso que Employee tenga un salario de salario de 400F con una moneda en Euros, un porcentaje de bonificacion del 80% y sea un Supervisor
 	@Test
 	void testCs2() {
 		Employee e2 = new Employee(400F,"EUR",0.8F,EmployeeType.Supervisor);
 		float salario = e2.cs();
 		assertEquals(444.61F,salario,0.01F);
 	}
-	
+	// Test en caso que Employee tenga un salario de salario de 400F con una moneda en Euros, un porcentaje de bonificacion del 80% y sea un Manager
 	@Test
 	void testCs3() {
 		Employee e3 = new Employee(400F,"EUR",0.8F,EmployeeType.Manager);
 		float salario = e3.cs();
 		assertEquals(444.89F,salario,0.01F);
 	}
-	
+	// Test en caso que Employee tenga un salario de salario de 400F con una moneda en Dolares, un porcentaje de bonificacion del 80% y sea un trabajador
 	@Test
 	void testCs4() {
 		Employee e4 = new Employee(400F,"USD",0.8F,EmployeeType.Worker);
 		float salario = e4.cs();
 		assertEquals(464.33F,salario,0.01F);
 	}
-	
+	// Test en caso que Employee tenga un salario de salario de 400F con una moneda en Dolares, un porcentaje de bonificacion del 80% y sea un Supervisor
 	@Test
 	void testCs5() {
 		Employee e5 = new Employee(400F,"USD",0.8F,EmployeeType.Supervisor);
 		float salario = e5.cs();
 		assertEquals(464.61F,salario,0.01F);
 	}
-	
+	// Test en caso que Employee tenga un salario de salario de 400F con una moneda en Dolares, un porcentaje de bonificacion del 80% y sea un Manager
 	@Test
 	void testCs6() {
 		Employee e6 = new Employee(400F,"USD",0.8F,EmployeeType.Manager);
@@ -53,23 +55,23 @@ class EmployeeTest {
 		assertEquals(464.89F,salario,0.01F);
 	}
 	//Calculate the bonus of the employee depended on the currency and the type of employee
-//Test para Worker con currency USD
+	
+	//Test para Worker con currency USD
 	@Test
 	void testCalculateYearBonusWorkerUSD() {
-	float bonusPercentage = (float) 0.1;
-	float salary =1500;
-	String currency ="USD";
-	EmployeeType etype=EmployeeType.Worker;
-	Employee emp =
-			new Employee(salary,currency,bonusPercentage,etype);
-	float expected =386;
-	float experimental = emp.CalculateYearBonus();
-	assertEquals(expected,experimental,0.1);
+		float bonusPercentage = (float) 0.1;
+		float salary =1500;
+		String currency ="USD";
+		EmployeeType etype = EmployeeType.Worker;
+		Employee emp =	new Employee(salary,currency,bonusPercentage,etype);
+		float expected =386;
+		float experimental = emp.CalculateYearBonus();
+		assertEquals(expected,experimental,0.1);
 
 
-}
+	}
 
-//Test para Worker con currency USD
+	//Test para Worker con currency USD
 	@Test
 	void testCalculateYearBonusWorkerELSE() {
 		float bonusPercentage = (float) 0.1;
